41e373210d6f6ca696c6ffbd2b4c66a8b9e26fc0
pxphbn8 <160334114+pxphbn8@users.noreply.github.com>
2025-01-02 02:22:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -29,13 +29,19 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// // Kết nối đến cơ sở dữ liệu MongoDB
-// const MONGO_URI = 'mongodb://localhost:27017/ToDoListDB';
-// mongoose.connect(MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
- 
+
+
+// mongoose.connect('mongodb+srv://vuducluong12a:123@cluster0.kznsm.mongodb.net/database?retryWrites=true&w=majority&appName=Cluster0');
+
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.once('open', () => {
+//   console.log('Connected to MongoDB');
 // });
+
+
+
+
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
@@ -47,7 +53,6 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB');
 });
-
 
 
 
