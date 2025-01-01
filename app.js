aba97e536b2ab2afce157820af2a9b38cf7a23c8
pxphbn8 <160334114+pxphbn8@users.noreply.github.com>
2025-01-02 02:31:01 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from "body-parser";
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+// Import các routes
 import taskRoutes from './routes/taskRoute.js'; 
 import homeRoutes from './routes/homeRoute.js';
 import importantRoutes from './routes/importantRoute.js';
@@ -13,51 +14,33 @@ import signupRoutes from './routes/signupRoute.js';
 import todayRoutes from './routes/todayRoute.js';
 import searchRoutes from './routes/searchRoute.js';
 import commentRoutes from './routes/commentRoute.js';
-import Routes from './routes/uploadFileRoute.js';
-import router from './routes/forgotpasswordRoute.js';
-import dotenv from 'dotenv';
+import uploadFileRoutes from './routes/uploadFileRoute.js';
+import forgotPasswordRoutes from './routes/forgotpasswordRoute.js';
+
+// Load biến môi trường từ file .env
 dotenv.config();
 
 const app = express();
 
-
-// Cấu hình CORS theo nhu cầu của bạn
+// Cấu hình CORS
 app.use(cors());
 
-
-// Parse request bodies (req.body)
+// Middleware để parse body của request
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Kết nối đến MongoDB
+const mongoURI = process.env.MONGO_URI;
+if (!mongoURI) {
+  console.error("MongoDB URI không được cấu hình trong .env");
+  process.exit(1);
+}
 
+mongoose.connect(mongoURI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
-// mongoose.connect('mongodb+srv://vuducluong12a:123@cluster0.kznsm.mongodb.net/database?retryWrites=true&w=majority&appName=Cluster0');
-
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-// db.once('open', () => {
-//   console.log('Connected to MongoDB');
-// });
-
-
-
-
-
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
-
-
-
-
-// Sử dụng router cho các endpoint của Task
+// Đăng ký các routes
 app.use('/api/tasks', taskRoutes);
 app.use('/api/home', homeRoutes);
 app.use('/api/important', importantRoutes);
@@ -67,14 +50,11 @@ app.use('/api/signup', signupRoutes);
 app.use('/api/today', todayRoutes);
 app.use('/api/search', searchRoutes);
 app.use('/api/comments', commentRoutes);
-app.use('/api/uploadfile', Routes);
-app.use('/api/auth', router);
+app.use('/api/uploadfile', uploadFileRoutes);
+app.use('/api/auth', forgotPasswordRoutes);
 
-
-
+// Start server
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-
