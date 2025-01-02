18329ecf80e16ffa6bcaa9737c602a6c40343d25
pxphbn8 <160334114+pxphbn8@users.noreply.github.com>
2025-01-02 16:48:55 +0700

    Update CommentModel.js

--- models/CommentModel.js
+++ models/CommentModel.js
@@ -1,13 +1,13 @@
-// models/Comment.js
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const commentSchema = new mongoose.Schema(
   {
     text: { type: String, required: true },
-    user: { type: String, required: true }, // Thông tin người bình luận
-    createdAt: { type: Date, default: Date.now }
+    user: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model('Comment', commentSchema);
+const Comment = mongoose.model('Comment', commentSchema);
+export default Comment;
