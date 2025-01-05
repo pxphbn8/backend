f5247ff0d10678ab4d9f966ad019718298231906
pxphbn8 <160334114+pxphbn8@users.noreply.github.com>
2025-01-05 14:24:43 +0700

    Update commentRoute.js

--- routes/commentRoute.js
+++ routes/commentRoute.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import { getAllComments, addComment, deleteComment } from '../controllers/commentController.js';
 
-const router = express.Router();
+const commentRoutes = express.Router();
 
-// Định nghĩa các endpoint
-router.get('/', getAllComments);
-router.post('/', addComment);
-router.delete('/:id', deleteComment);
+// Endpoint lấy tất cả các bình luận của user
+commentRoutes.get('/getAllComments/:userID', getAllComments);
 
-export default router;
+// Endpoint thêm bình luận mới
+commentRoutes.post('/addComment/:userID', addComment);
+
+// Endpoint xóa bình luận theo ID
+commentRoutes.delete('/deleteComment/:id', deleteComment);
+
+export default commentRoutes;
+
